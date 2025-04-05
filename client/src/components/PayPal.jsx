8d1985e93e450803234d8d177c2fb231af2cec32
Mehulgarg002 <mehulgarg020@gmail.com>
2025-04-05 14:34:24 +0530

    Initial commit

---
+++ client/src/components/PayPal.jsx
@@ -0,0 +1,13 @@
+// import React, { useEffect, useRef } from 'react'
+
+// const paypal = useRef()
+
+// useEffect(()=>{
+//   window.paypal.Buttons
+// },[])
+
+// export default function PayPal() {
+//   return (
+//     <div ref={paypal}></div>
+//   )
+// }
