6e27937eae4d22e2dcc28cf3c7dda02823fa527e
Mehulgarg002 <115608099+Mehulgarg002@users.noreply.github.com>
2025-04-18 16:39:52 +0530

    Update database.js

--- api/config/database.js
+++ api/config/database.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const MongoConnect = () => {
-  mongoose.connect(process.env.MONGODB);
+  mongoose.connect("mongodb+srv://ashutoshs97:ashutoshs97@flexihire.de9icog.mongodb.net/FlexDB?retryWrites=true&w=majority");
   const db = mongoose.connection;
   db.on("error", (err) => {
     console.log("Database Connection Error: " + err.message);
